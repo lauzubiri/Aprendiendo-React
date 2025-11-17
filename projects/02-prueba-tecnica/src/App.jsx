0d6717b1d8462790e3ecf731a13a8fa5e1fc901c
Lautaro Zubiri <lauzubiri@gmail.com>
2025-11-17 15:18:51 -0300

    Avances en el proyecto de prueba tecnica

--- projects/02-prueba-tecnica/src/App.jsx
+++ projects/02-prueba-tecnica/src/App.jsx
@@ -2,27 +2,48 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const ENDPOINT_CAT_FACT = ('https://catfact.ninja/fact')
-//const ENDPOINT_CAT_IMAGE_URL = (`zhttps://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`)
-
 
 function App () {
 
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   
   useEffect(() => 
   {
     fetch(ENDPOINT_CAT_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
-  }, []
-  )
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  },
+  [])
+  
+  useEffect(() => {
+
+    if (!fact) return
+
+    const firstWord = fact.split(' ', 3).join(' ')
+    console.log({firstWord}) 
+      
+    fetch(`https://cataas.com/cat/says/${firstWord}?fontSize=40&fontColor=white&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
   
   return (
     <>
-      <main>
-        <h1>App de gatitos!</h1>
-        {fact && <p>{fact}</p>}
+      <main className='flex flex-col place-items-center gap-8'>
+        <h1 className='text-3xl'>App de gatitos!</h1>
+        {fact && <p className='text-center'>{fact}</p>}
+        {imageUrl && <img
+          className='max-w-96 h-auto'
+          src={imageUrl} alt={`Imagen generada con las primeras tres palabras de ${fact}`
+          } />}
       </main>
     </>
   )
